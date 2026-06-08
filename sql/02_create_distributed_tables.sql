6a1407c2f766d27b2ba15aa194c572d6192a9a42
mynameisFuong <n23dccn047@student.ptithcm.edu.vn>
2026-06-08 23:28:05 +0700

    update report

--- sql/02_create_distributed_tables.sql
+++ sql/02_create_distributed_tables.sql
@@ -4,6 +4,7 @@ SET search_path TO ecommerce_dw;
 -- Fact table: horizontally sharded by order_id.
 -- All items of the same order stay on the same worker shard.
 -- Replication factor 2 creates two placements for each shard across the worker pool.
+SET citus.shard_count = 32;
 SET citus.shard_replication_factor = 2;
 SELECT create_distributed_table('ecommerce_dw.fact_olist_orders', 'order_id');
 
